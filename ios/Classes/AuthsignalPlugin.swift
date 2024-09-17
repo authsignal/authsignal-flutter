ea96dc19b3e8a36002f2b3dc9967a7f5a159b77f
Chris Fisher <chris.i.fisher@gmail.com>
2024-09-17 14:42:00 +1200

    Upgrade to latest iOS/Android SDKs

--- ios/Classes/AuthsignalPlugin.swift
+++ ios/Classes/AuthsignalPlugin.swift
@@ -28,16 +28,24 @@ public class AuthsignalPlugin: NSObject, FlutterPlugin {
     case "passkey.signUp":
       let arguments = call.arguments as! [String: Any]
       let token = arguments["token"] as! String
-      let userName = arguments["userName"] as? String
+      let username = arguments["username"] as? String
+      let displayName = arguments["displayName"] as? String
 
       Task.init {
-        let response = await self.passkey!.signUp(token: token, userName: userName)
+        let response = await self.passkey!.signUp(token: token, username: username)
         
-        if (response.error != nil) {
-          let error = FlutterError(code: "signUpError", message: response.error, details: "");
+        if response.errorCode == "TOKEN_NOT_SET" {
+          let error = FlutterError(code: "tokenNotSetError", message: "TOKEN_NOT_SET", details: "")
+          result(error)
+        } else if response.error != nil {
+          let error = FlutterError(code: "signUpError", message: response.error, details: "")
           result(error)
         } else {
-          result(response.data)
+          let data: [String: Any?] = [
+            "token": response.data!.token,
+          ]
+
+          result(data)
         }
       }
       
@@ -46,15 +54,33 @@ public class AuthsignalPlugin: NSObject, FlutterPlugin {
       let action = arguments["action"] as? String
       let token = arguments["token"] as? String
       let autofill = arguments["autofill"] as? Bool ?? false
+      let preferImmediatelyAvailableCredentials = arguments["preferImmediatelyAvailableCredentials"] as? Bool ?? true
 
       Task.init {
-        let response = await self.passkey!.signIn(token: token, action: action, autofill: autofill)
+        let response = await self.passkey!.signIn(
+          token: token,
+          action: action,
+          autofill: autofill,
+          preferImmediatelyAvailableCredentials: preferImmediatelyAvailableCredentials
+        )
         
-        if (response.error != nil) {
-          let error = FlutterError(code: "signInError", message: response.error, details: "");
+        if (response.errorCode == "SIGN_IN_CANCELED") {
+          let error = FlutterError(code: "signInCanceled", message: "SIGN_IN_CANCELED", details: "")
+          result(error)
+        } else if response.error != nil {
+          let error = FlutterError(code: "signInError", message: response.error, details: "")
           result(error)
         } else {
-          result(response.data)
+          let data: [String: Any?] = [
+            "isVerified": response.data!.isVerified,
+            "token": response.data!.token,
+            "userId": response.data!.userId,
+            "userAuthenticatorId": response.data!.userAuthenticatorId,
+            "username": response.data!.username,
+            "displayName": response.data!.displayName,
+          ]
+
+          result(data)
         }
       }
 
@@ -65,7 +91,7 @@ public class AuthsignalPlugin: NSObject, FlutterPlugin {
       Task.init {
         let response = await self.passkey!.isAvailableOnDevice()
         
-        if (response.error != nil) {
+        if response.error != nil {
           let error = FlutterError(code: "isAvailableOnDeviceError", message: response.error, details: "");
           result(error)
         } else {
@@ -86,22 +112,33 @@ public class AuthsignalPlugin: NSObject, FlutterPlugin {
       Task.init {
         let response = await self.push!.getCredential()
         
-        if (response.error != nil) {
+        if response.error != nil {
           let error = FlutterError(code: "getCredentialError", message: response.error, details: "");
           result(error)
+        } else if let data = response.data {
+          let credential: [String: String?] = [
+            "credentialId": data.credentialId,
+            "createdAt": data.createdAt,
+            "lastAuthenticatedAt": data.lastAuthenticatedAt,
+          ]
+
+          result(data)
         } else {
-          result(response.data)
+          result(nil)
         }
       }
           
     case "push.addCredential":
       let arguments = call.arguments as! [String: Any]
-      let token = arguments["token"] as! String
+      let token = arguments["token"] as? String
 
       Task.init {
         let response = await self.push!.addCredential(token: token)
         
-        if (response.error != nil) {
+        if response.errorCode == "TOKEN_NOT_SET" {
+          let error = FlutterError(code: "tokenNotSetError", message: "TOKEN_NOT_SET", details: "")
+          result(error)
+        } else if response.error != nil {
           let error = FlutterError(code: "addCredentialError", message: response.error, details: "");
           result(error)
         } else {
@@ -113,7 +150,7 @@ public class AuthsignalPlugin: NSObject, FlutterPlugin {
       Task.init {
         let response = await self.push!.removeCredential()
         
-        if (response.error != nil) {
+        if response.error != nil {
           let error = FlutterError(code: "removeCredentialError", message: response.error, details: "");
           result(error)
         } else {
@@ -125,28 +162,39 @@ public class AuthsignalPlugin: NSObject, FlutterPlugin {
       Task.init {
         let response = await self.push!.getChallenge()
         
-        if (response.error != nil) {
+        if response.error != nil {
           let error = FlutterError(code: "getChallengeError", message: response.error, details: "");
           result(error)
+        } else if let challenge = response.data as? PushChallenge {
+          let data: [String: String?] = [
+            "challengeId": challenge.challengeId,
+            "actionCode": challenge.actionCode,
+            "idempotencyKey": challenge.idempotencyKey,
+            "userAgent": challenge.userAgent,
+            "deviceId": challenge.deviceId,
+            "ipAddress": challenge.ipAddress,
+          ]
+
+          result(data)
         } else {
-          result(response.data ?? nil)
+          result(nil)
         }
       }
       
     case "push.updateChallenge":
       let arguments = call.arguments as! [String: Any]
-      let challengeID = arguments["challengeId"] as! String
+      let challengeId = arguments["challengeId"] as! String
       let approved = arguments["approved"] as! Bool
       let verificationCode = arguments["verificationCode"] as? String
       
       Task.init {
         let response = await self.push!.updateChallenge(
-          challengeID: challengeID,
+          challengeId: challengeId,
           approved: approved,
           verificationCode: verificationCode
         )
         
-        if (response.error != nil) {
+        if response.error != nil {
           let error = FlutterError(code: "updateChallengeError", message: response.error, details: "");
           result(error)
         } else {
